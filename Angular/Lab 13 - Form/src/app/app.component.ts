81036a61f585d53d18b302de657bd9a6f4688e9e
Rufus Immanuel Raj P <rufus5802@gmail.com>
2022-11-21 00:11:10 +0530

    Add Angular programs (Lab 10, 12, 13)
    idk any of these :(

---
+++ Angular/Lab 13 - Form/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Print Data';
+  userData:any = {};
+  getData(data:NgForm) {
+    console.warn(data);
+    this.userData=data
+  }
+}
